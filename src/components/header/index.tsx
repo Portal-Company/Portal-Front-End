b61ac77ed1e3178f13e304b52b857dd7a35abc45
menezescosta <mc611641@gmail.com>
2023-01-31 17:12:23 +0100

    feat: add header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './styles'
+
+const Header=()=>{
+    return(
+        <S.Container>
+            Header
+        </S.Container>
+    )
+}
+
+export default Header;
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-//import Header from '../header'
+import Header from '../header'
 import * as S from './styles'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
-//<Header />
 export const Layout = ({ children }: LayoutProps) => {
   return (
     <S.Wrapper>
       <S.Content>
+        <Header />
         <S.Main>{children}</S.Main>
       </S.Content>
     </S.Wrapper>
