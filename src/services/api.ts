a947d81aa44d4036b173e85349145168f8d05607
ercabsalias <ercabsalias@gmail.com>
2023-04-11 09:37:12 +0100

    Link da Hospedagem

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
-
-import axios from 'axios'
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:4444'
-})
+  baseURL: "https://portal-das-escolas-api.onrender.com",
+});
