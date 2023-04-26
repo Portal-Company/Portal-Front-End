3056a2f4f7d5d7ce2beaeb8e6805e44217484d7e
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-04-26 12:36:32 +0100

    fix: resolvendo o bug

--- src/views/School-Profile/index.tsx
+++ src/views/School-Profile/index.tsx
@@ -14,9 +14,6 @@ interface Props {
 }
 
 export const SchoolProfileView: React.FC<Props> = ({ school }) => {
-  const { data, error } = useSWR("/file/", () => fetchData(school));
-  const router = useRouter();
-
   const {
     query: { id },
   } = useRouter();
@@ -28,122 +25,137 @@ export const SchoolProfileView: React.FC<Props> = ({ school }) => {
     });
   };
 
+  // const { data, error} = useSWR('/file/', () => fetchData(school))
+  const { data: fotoUrl } = useFetch(`/file/${school?.fotoUrl}`);
+  const { data: logoUrl } = useFetch(`/file/${school?.logo}`);
 
-    const { data, error} = useSWR('/file/', () => fetchData(school))  
-    const { data: fotoUrl} = useFetch(`/file/${school?.fotoUrl}`)
-    const { data: logoUrl} = useFetch(`/file/${school?.logo}`)
+  const router = useRouter();
 
-    const router = useRouter()
-    
-    return(
-        <>
-        <S.Container>
-            <S.Content>
-                <S.FirstSection>
-                    <S.ContainerImage>
-                        {fotoUrl ? ( <Image src={fotoUrl?.link} alt="escola" width={490} height={500} loading="lazy" />) : null}
-                    </S.ContainerImage>
-                    <S.ContainerLeft>
-                        <S.Title>
-                            {school?.nome}     
-                        </S.Title>
-                        <S.SectionDescription>
-                            <span>Localização: </span>
-                            <span> {school?.Localizacao?.endereco1} / {school?.Localizacao?.Provincia.nome}</span>
-                        </S.SectionDescription>
-                        <S.SectionDescription>
-                            <span>Categoria: {school?.Categoria?.nome}</span>
-                        </S.SectionDescription>
-                        <S.ContainerLeftFooter>
-                            <section>
-                                <span>Logo: </span>
-                                {logoUrl ? (<Image src={logoUrl?.link} alt="escola" width={490} height={500} loading="lazy" />) : null} 
-                            </section>
-                            <section>
-                                <S.ButtonSubscribe>Inscrever-se</S.ButtonSubscribe>
-                            </section>
-                        </S.ContainerLeftFooter>
-                    </S.ContainerLeft>
-                </S.FirstSection>
-                <S.SecondSection>
-                    <S.SecondSectionChild1>
-                        <S.Title>
-                            Historial
-                        </S.Title>
-                        <S.SectionDescription>
-                            <div>
-                                    <span 
-                                        style={{marginRight: "0.5rem"}}
-                                    >
-                                        Fundador: {school?.historial?.fundador} 
-                                    </span>
-                            </div>
-                            <span>
-                                Ano:  {school?.historial?.data}
-                            </span>
-                        </S.SectionDescription>
-                        <S.SectionDescription>
-                            <span>Breve História: </span>
-                        </S.SectionDescription>
-                        <S.ContentStory>
-                            <S.DescriptionStory>
-                                {school?.historial?.descricao}
-                            </S.DescriptionStory>
-                        </S.ContentStory>
-                    </S.SecondSectionChild1>
-                    <S.SecondSectionChild2>
-                        <div>
+  return (
+    <>
+      <S.Container>
+        <S.Content>
+          <S.FirstSection>
+            <S.ContainerImage>
+              {fotoUrl ? (
+                <Image
+                  src={fotoUrl?.link}
+                  alt="escola"
+                  width={490}
+                  height={500}
+                  loading="lazy"
+                />
+              ) : null}
+            </S.ContainerImage>
+            <S.ContainerLeft>
+              <S.Title>{school?.nome}</S.Title>
+              <S.SectionDescription>
+                <span>Localização: </span>
+                <span>
+                  {" "}
+                  {school?.Localizacao?.endereco1} /{" "}
+                  {school?.Localizacao?.Provincia.nome}
+                </span>
+              </S.SectionDescription>
+              <S.SectionDescription>
+                <span>Categoria: {school?.Categoria?.nome}</span>
+              </S.SectionDescription>
+              <S.ContainerLeftFooter>
+                <section>
+                  <span>Logo: </span>
+                  {logoUrl ? (
+                    <Image
+                      src={logoUrl?.link}
+                      alt="escola"
+                      width={490}
+                      height={500}
+                      loading="lazy"
+                    />
+                  ) : null}
+                </section>
+                <section>
+                  <S.ButtonSubscribe>Inscrever-se</S.ButtonSubscribe>
+                </section>
+              </S.ContainerLeftFooter>
+            </S.ContainerLeft>
+          </S.FirstSection>
+          <S.SecondSection>
+            <S.SecondSectionChild1>
+              <S.Title>Historial</S.Title>
+              <S.SectionDescription>
+                <div>
+                  <span style={{ marginRight: "0.5rem" }}>
+                    Fundador: {school?.historial?.fundador}
+                  </span>
+                </div>
+                <span>Ano: {school?.historial?.data}</span>
+              </S.SectionDescription>
+              <S.SectionDescription>
+                <span>Breve História: </span>
+              </S.SectionDescription>
+              <S.ContentStory>
+                <S.DescriptionStory>
+                  {school?.historial?.descricao}
+                </S.DescriptionStory>
+              </S.ContentStory>
+            </S.SecondSectionChild1>
+            <S.SecondSectionChild2>
+              {/* <div>
                             {data?.historial1?.link ? <Image onClick={() => router.push(data.historial1?.link)} loader={() => data.historial1.link} src={data.historial1.link} width={100} height={90} alt="escola"  layout="responsive"/> : null} 
                         </div>
                         <div>
                             {data?.historial2?.link ? <Image onClick={() => router.push(data.historial2?.link)} loader={() => data.historial2.link} src={data.historial2.link} width={100} height={90} alt="escola"  layout="responsive"/> : null} 
                         </div>
-                        <div>
+                        <div> 
                             {data?.historial3?.link ? <Image onClick={() => router.push(data.historial3?.link)} loader={() => data.historial3.link} src={data.historial3.link} width={100} height={90} alt="escola" layout="responsive" /> : null} 
                         </div>
                         <div>
                             {data?.historial4?.link ? <Image onClick={() => router.push(data.historial4?.link)} loader={() => data.historial4.link} src={data.historial4.link} width={100} height={90} alt="escola" layout="responsive" /> : null} 
-                        </div>
-                    </S.SecondSectionChild2>
-                </S.SecondSection>
-                <S.ThirdSection>
-                    <S.Title>
-                        Áreas de Formação
-                    </S.Title>
-                    <S.ContainerCard>
-                        {school.areaDeFormacao?.map((area) => (
-                            <Card key={area.id} content={area} onClick = {() => router.push(`/SearchSchool/AreaDeFormacao/${area.id}`)} />
-                        ))}
-                    </S.ContainerCard>
-                </S.ThirdSection>
-                <S.FourthSection>
-                    <S.Title>
-                        Actividades Anuais
-                    </S.Title>
-                    <S.ContainerCard>
-                        {school?.actividade.map((activity) => (
-                            <Card key={activity.id} content={activity}  onClick={() => router.push(`/SearchSchool/Actividades/${activity.id}`)}/>
-                        ))}
-                    </S.ContainerCard>
-                </S.FourthSection>
-                <S.FifthSection>
-                    {school.Organigrama.Departamento.map((data) => (
-                        <>
-                        <S.Title>
-                            {data.nome}
-                        </S.Title>
-                        <S.ContainerCard>
-                            {data.Funcionario.map((data) =>(
-                                <Card key={data.id} content={data}/>
-                            ))}
-                        </S.ContainerCard>
-                        </>
-                    ))}
-                </S.FifthSection>
-                
-            </S.Content>
-            <Footer/>
-        </S.Container>
-        </>
-    );
-}  
+                        </div> */}
+            </S.SecondSectionChild2>
+          </S.SecondSection>
+          <S.ThirdSection>
+            <S.Title>Áreas de Formação</S.Title>
+            <S.ContainerCard>
+              {school.areaDeFormacao?.map((area) => (
+                <Card
+                  key={area.id}
+                  content={area}
+                  onClick={() =>
+                    router.push(`/SearchSchool/AreaDeFormacao/${area.id}`)
+                  }
+                />
+              ))}
+            </S.ContainerCard>
+          </S.ThirdSection>
+          <S.FourthSection>
+            <S.Title>Actividades Anuais</S.Title>
+            <S.ContainerCard>
+              {school?.actividade.map((activity) => (
+                <Card
+                  key={activity.id}
+                  content={activity}
+                  onClick={() =>
+                    router.push(`/SearchSchool/Actividades/${activity.id}`)
+                  }
+                />
+              ))}
+            </S.ContainerCard>
+          </S.FourthSection>
+          <S.FifthSection>
+            {school.Organigrama.Departamento.map((data) => (
+              <>
+                <S.Title>{data.nome}</S.Title>
+                <S.ContainerCard>
+                  {data.Funcionario.map((data) => (
+                    <Card key={data.id} content={data} />
+                  ))}
+                </S.ContainerCard>
+              </>
+            ))}
+          </S.FifthSection>
+        </S.Content>
+      </S.Container>
+    </>
+  );
+};
