562aa236f0c001a4f06a1dc16a59256fa37e19e6
menezescosta <mc611641@gmail.com>
2023-05-31 15:57:18 +0100

    feat: alterar estilo

--- src/components/oferta-formtiva/index.tsx
+++ src/components/oferta-formtiva/index.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 import * as S from "./styles";
 import Image from "next/image";
-
 import { CiGlobe } from "react-icons/ci";
-
 import Button from "../button/button";
 import Link from "next/link";
 const OfertaFormativa: React.FC = () => {
@@ -11,9 +9,23 @@ const OfertaFormativa: React.FC = () => {
     <S.Container>
       <S.Title>Conheça a nossa Oferta Formativa</S.Title>
       <S.Card>
-        <Image src="/assets/m6.jpg" alt="IMG" width={600} height={400} />
+        <Image
+          src="/assets/engineer-cooperation-male-female.jpg"
+          alt="IMG"
+          width={550}
+          height={350}
+        />
         <S.CardText data-aos="fade-down">
           <h2>Engenharias e Tecnologias</h2>
+          <p>
+            Oportunidades de carreira: Os cursos de engenharia e tecnologia
+            proporcionam uma base sólida para uma variedade de carreiras em
+            campos como engenharia civil, engenharia mecânica, engenharia
+            elétrica, ciência da computação, tecnologia da informação, entre
+            outros. Essas áreas oferecem uma demanda contínua por profissionais
+            qualificados, com muitas oportunidades de emprego e crescimento
+            profissional.
+          </p>
           <Link href="/SearchSchool">
             <Button Icon={<CiGlobe />} textName="Ver Escolas" />
           </Link>
@@ -22,11 +34,24 @@ const OfertaFormativa: React.FC = () => {
       <S.Card>
         <S.CardText data-aos="zoom-in">
           <h2>Ciências Sociais Aplicadas</h2>
+          <p>
+            Os cursos de ciências, como biologia, química, física e ciências da
+            terra, fornecem uma compreensão mais profunda dos princípios e leis
+            que governam o mundo natural. Os alunos aprendem sobre os
+            fundamentos da vida, matéria, energia, forças naturais e processos
+            geológicos. Essa compreensão científica ajuda a explicar o
+            funcionamento do mundo ao nosso redor.
+          </p>
           <Link href="/SearchSchool">
             <Button Icon={<CiGlobe />} textName="Ver Escolas" />
           </Link>
         </S.CardText>
-        <Image src="/assets/m6.jpg" alt="IMG" width={600} height={400} />
+        <Image
+          src="/assets/african-amercan.jpg"
+          alt="IMG"
+          width={550}
+          height={350}
+        />
       </S.Card>
     </S.Container>
   );
